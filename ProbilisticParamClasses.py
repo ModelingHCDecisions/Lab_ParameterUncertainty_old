723fa7b512de7a0c4880894764e59af7b8e9451b
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2023-04-24 20:20:55 -0400

    2023

--- ProbilisticParamClasses.py
+++ ProbilisticParamClasses.py
@@ -1,8 +1,3 @@
-import math
-
-import scipy.stats as stat
-
-import deampy.random_variates as rvgs
 from EconEvalParamClasses import *  # import everything from the EconEvalParamClasses module
 
 
@@ -33,63 +28,34 @@ class ParameterGenerator:
         self.annualTreatmentCostRVG = None   # gamma distribution for treatment cost
 
         # create Dirichlet distributions for transition probabilities
-        for probs in Data.TRANS_MATRIX:
-            # note:  for a Dirichlet distribution all values of the argument 'a' should be non-zero.
-            # setting if_ignore_0s to True allows the Dirichlet distribution to take 'a' with zero values.
-            self.probMatrixRVG.append(rvgs.Dirichlet(
-                a=probs, if_ignore_0s=True))
 
         # treatment relative risk
         rr_ci = [0.365, 0.71]   # confidence interval of the treatment relative risk
 
         # find the mean and st_dev of the normal distribution assumed for ln(RR)
         # sample mean ln(RR)
-        mean_ln_rr = math.log(Data.TREATMENT_RR)
+
         # sample standard deviation of ln(RR)
-        std_ln_rr = \
-            (math.log(rr_ci[1]) - math.log(rr_ci[0])) / (2 * stat.norm.ppf(1 - 0.05 / 2))
+
         # create a normal distribution for ln(RR)
-        self.lnRelativeRiskRVG = rvgs.Normal(loc=mean_ln_rr,
-                                             scale=std_ln_rr)
 
         # create gamma distributions for annual state cost
-        for cost in Data.ANNUAL_STATE_COST:
 
             # if cost is zero, add a constant 0, otherwise add a gamma distribution
-            if cost == 0:
-                self.annualStateCostRVG.append(rvgs.Constant(value=0))
-            else:
+
                 # find shape and scale of the assumed gamma distribution
                 # no data available to estimate the standard deviation, so we assumed st_dev=cost / 5
-                fit_output = rvgs.Gamma.fit_mm(mean=cost, st_dev=cost / 5)
+
                 # append the distribution
-                self.annualStateCostRVG.append(
-                    rvgs.Gamma(a=fit_output["a"],
-                               loc=0,
-                               scale=fit_output["scale"]))
 
         # create a gamma distribution for annual treatment cost
-        if self.therapy == Therapies.MONO:
-            annual_cost = Data.Zidovudine_COST
-        else:
-            annual_cost = Data.Zidovudine_COST + Data.Lamivudine_COST
-        fit_output = rvgs.Gamma.fit_mm(mean=annual_cost, st_dev=annual_cost / 5)
-        self.annualTreatmentCostRVG = rvgs.Gamma(a=fit_output["a"],
-                                                 loc=0,
-                                                 scale=fit_output["scale"])
 
         # create beta distributions for annual state utility
-        for utility in Data.ANNUAL_STATE_UTILITY:
+
             # if utility is zero, add a constant 0, otherwise add a beta distribution
-            if utility == 0:
-                self.annualStateCostRVG.append(rvgs.Constant(value=0))
-            else:
+
                 # find alpha and beta of the assumed beta distribution
                 # no data available to estimate the standard deviation, so we assumed st_dev=cost / 4
-                fit_output = rvgs.Beta.fit_mm(mean=utility, st_dev=utility / 4)
-                # append the distribution
-                self.annualStateUtilityRVG.append(
-                    rvgs.Beta(a=fit_output["a"], b=fit_output["b"]))
 
     def get_new_parameters(self, seed):
         """
@@ -107,35 +73,26 @@ class ParameterGenerator:
         # for all health states
         for s in HealthStates:
             # if the current state is not death
-            if s not in [HealthStates.HIV_DEATH, HealthStates.NATUAL_DEATH]:
+
                 # sample from the dirichlet distribution to find the transition probabilities between hiv states
                 # fill in the transition probabilities out of this state
-                prob_matrix.append(self.probMatrixRVG[s.value].sample(rng))
+
 
         # sampled relative risk
-        rr = math.exp(self.lnRelativeRiskRVG.sample(rng))
 
         # calculate transition probabilities between hiv states
         if self.therapy == Therapies.MONO:
             # calculate transition probability matrix for the mono therapy
-            param.transRateMatrix = get_trans_rate_matrix(trans_prob_matrix=prob_matrix)
+
 
         elif self.therapy == Therapies.COMBO:
             # calculate transition probability matrix for the combination therapy
-            param.transRateMatrix = get_trans_rate_matrix_combo(
-                rate_matrix_mono=get_trans_rate_matrix(trans_prob_matrix=prob_matrix),
-                combo_rr=rr)
 
         # sample from gamma distributions that are assumed for annual state costs
-        for dist in self.annualStateCostRVG:
-            param.annualStateCosts.append(dist.sample(rng))
 
         # sample from the gamma distribution that is assumed for the treatment cost
-        param.annualTreatmentCost = self.annualTreatmentCostRVG.sample(rng)
 
         # sample from beta distributions that are assumed for annual state utilities
-        for dist in self.annualStateUtilityRVG:
-            param.annualStateUtilities.append(dist.sample(rng))
 
         # return the parameter set
         return param
